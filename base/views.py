9cd136779446737433384d19ccec3177aa200960
Rudaba-Kamor <rudabaadninkamor@gmail.com>
2023-07-17 23:06:30 +0600

    project upload

---
+++ base/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html') 
+
+def about(request):
+    return render(request, 'about.html') 
+
+def blog(request):
+    return render(request, 'blog.html') 
+
+def dashboard(request):
+    return render(request, 'dashboard.html') 
+
+def contact(request):
+    return render(request, 'contact.html') 
+
